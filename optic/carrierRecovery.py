c48f54f1e89ada6a826afca101d71bace55cefe2
valmir <valmir@goclever.io>
2022-11-30 15:31:02 -0300

    refactor: extracts sub functions from bps

--- optic/carrierRecovery.py
+++ optic/carrierRecovery.py
@@ -91,6 +91,21 @@ def gen_test_phases(num_rotations: int) -> np.ndarray:
 
 
 @njit
+def compute_min_dist_to_constellation_symbols(symbol, const_symb):
+    dist_to_each_const_symbol = np.abs(symbol - const_symb) ** 2
+    return np.min(dist_to_each_const_symbol)
+
+@njit
+def get_optimum_phase_angle(ϕ_test, dmin):
+    """
+    The optimum phase angle is determined by searching the minimum sum of
+    distance values.
+    """
+    sumDmin = np.sum(dmin, axis=1)
+    optimum_phase_index = np.argmin(sumDmin)
+    return ϕ_test[optimum_phase_index]
+
+@njit
 def bps(Ei, N, constSymb, B):
     """
     Blind phase search (BPS) algorithm
@@ -124,21 +139,20 @@ def bps(Ei, N, constSymb, B):
         (zeroPad, Ei, zeroPad)
     )  # pad start and end of the signal with zeros
 
-    L = x.shape[0]
+    num_symbols_after_padding = x.shape[0]
 
     for n in range(0, nModes):
 
-        dist = np.zeros((B, constSymb.shape[0]), dtype="float")
+        # Minimum distance for each test phase
         dmin = np.zeros((B, 2 * N + 1), dtype="float")
 
-        for k in range(0, L):
+        for symbol_index in range(0, num_symbols_after_padding):
+            symbol = x[symbol_index, n]
             for indPhase, ϕ in enumerate(ϕ_test):
-                dist[indPhase, :] = np.abs(x[k, n] * np.exp(1j * ϕ) - constSymb) ** 2
-                dmin[indPhase, -1] = np.min(dist[indPhase, :])
-            if k >= 2 * N:
-                sumDmin = np.sum(dmin, axis=1)
-                indRot = np.argmin(sumDmin)
-                θ[k - 2 * N, n] = ϕ_test[indRot]
+                rotated_symbol = symbol * np.exp(1j * ϕ)
+                dmin[indPhase, -1] = compute_min_dist_to_constellation_symbols(rotated_symbol, constSymb)
+            if symbol_index >= 2 * N:
+                θ[symbol_index - 2 * N, n] = get_optimum_phase_angle(ϕ_test, dmin)
             dmin = np.roll(dmin, -1)
     return θ
 
